812ea04383696dce749ddb035a6d3de162bfc433
erikwtn <erik.wtn@gmail.com>
2024-04-23 11:06:41 +0100

    added script ready to generate head and boids

---
+++ GE2-Test-Unity/Assets/Ready.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ready : MonoBehaviour
+{
+	[SerializeField] private GameObject head, boid;
+	[SerializeField] private int size = 4;
+	[SerializeField] private float dist = 1f;
+	private SpineAnimator _spineAnimator;
+	private void Start()
+	{
+		var h = Instantiate(head, transform.position, Quaternion.identity, transform.parent);
+		_spineAnimator = h.GetComponent<SpineAnimator>();
+		
+		for (var i = 0; i < size; i++)
+		{
+			var newZ = transform.position.z - (h.transform.localScale.z * i) - dist;
+			var newPos = new Vector3(transform.position.x, transform.position.y, newZ);
+			var b = Instantiate(boid, newPos, Quaternion.identity, transform.parent);
+			_spineAnimator.bones[i] = b;
+		}
+	}
+}
